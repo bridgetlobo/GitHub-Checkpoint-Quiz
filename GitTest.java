63b246c8444c5e0dd4a09ef584c5717fa3190543
bridgetlobo <bridgettlobo@gmail.com>
2018-12-03 10:41:27 -0500

    Delete parameter
    String parameter was deleted

--- GitTest.java
+++ GitTest.java
@@ -5,10 +5,12 @@
 public class GitTest{
 	private String test;
 
-	public GitTest(String testt){
-		testt = "this is my github test";
+	public GitTest(){
 	}
 
+	public void setString(String test){
+		this.test = test;
+	}
 	public String toString(String last){
 		return last;
 	}
